49466ffe28b827277e83f59f58f1337cd68ba519
Frank Martyn <FrankMartyn1945@gmail.com>
2017-04-30 10:02:31 -0400

    Consolidated all the TestInstructionSet JUnit tests

---
+++ assemblerZ80_Test/src/assembler/TestInstructionSet.java
@@ -0,0 +1,12 @@
+package assembler;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestInstructionSet_1.class, TestInstructionSet_2.class, TestInstructionSet_3.class,
+		TestInstructionSet_4.class })
+public class TestInstructionSet {
+
+}
