ad2c5c481cd9f1a492301c867cc5ddb50f061d52
Frank Martyn <FrankMartyn1945@gmail.com>
2017-08-12 10:32:29 -0400

    Added more direct tests to add to file data

---
+++ assemblerZ80_Test/Code/Work/testADD.asm
@@ -0,0 +1,171 @@
+;	testADD.asm
+
+PSTRING		EQU		0852H
+NEWLINE		EQU		088CH
+DELAY		EQU		0CE1H
+COUT		EQU		0861H
+		
+			
+;ARG1_MAX	EQU		0
+;ARG2_MAX	EQU		100
+;		
+;NINE		EQU		10;
+
+			ORG		1800H
+Start:
+		LD		SP,TopOfStack
+		CALL	ClearBuffer
+		CALL	INIT
+		CALL	NEWLINE
+		
+		CALL	InitArgs				; set values to Zero
+
+Loop1:		
+		CALL	PutArgsinBuffer
+		CALL	PerformADD
+		CALL	LineBreak
+	
+;		CALL	IncArg1
+		LD		HL,arg1
+		INC		(HL)
+
+		JR		NZ,Loop1
+;		CALL	IncArg2
+
+		LD		HL,arg2
+		INC		(HL)
+		JR		NZ,Loop1
+Loop2:		
+
+		RST		38H
+		JP		Start
+;------------------------------------------		
+
+PerformADD:							; no carry
+		CALL	GetArg1
+		LD		B,A						; arg1 in B
+		CALL	GetArg2					; arg2 in ACC
+		SCF								; set carry Flag
+		CCF								; reset Carry Flag
+		ADD		A,B						; Add them
+		
+		
+RecordFlags:
+		PUSH	AF						; save Flag result
+		CALL	AddByteToBufferSpace	; result is in ACC
+		
+;sign flag		
+		POP		DE						; get flags so we can test
+		PUSH	DE						; save flags so we can test later
+		LD		A,31H					; Assume flag set
+		BIT		7,E						; test it
+		JP		NZ,SignFlagSet
+		DEC		A						; Not Set  - ASCII 0
+SignFlagSet:
+		CALL	AddToBuffer	
+		
+;zero flag
+		POP		DE						; get flags so we can test
+		PUSH		DE						; save flags so we can test later
+		LD		A,31H					; Assume flag set
+		BIT		6,E						; test it
+		JP		NZ,ZeroFlagSet
+		DEC		A						; Not Set  - ASCII 0
+ZeroFlagSet:
+		CALL	AddToBuffer	
+		
+;HalfCarry flag
+		POP		DE						; get flags so we can test
+		PUSH		DE						; save flags so we can test later
+		LD		A,31H					; Assume flag set
+		BIT		4,E						; test it
+		JP		NZ,HalfCarryFlagSet
+		DEC		A						; Not Set  - ASCII 0
+HalfCarryFlagSet:
+		CALL	AddToBuffer
+		
+;Overflow flag
+		POP		DE						; get flags so we can test
+		PUSH		DE						; save flags so we can test later
+		LD		A,31H					; Assume flag set
+		BIT		2,E						; test it
+		JP		NZ,OverflowFlagSet
+		DEC		A						; Not Set  - ASCII 0
+OverflowFlagSet:
+		CALL	AddToBuffer	
+
+;N flag
+		POP		DE						; get flags so we can test
+		PUSH		DE						; save flags so we can test later
+		LD		A,31H					; Assume flag set
+		BIT		1,E						; test it
+		JP		NZ,nFlagSet
+		DEC		A						; Not Set  - ASCII 0
+nFlagSet:
+		CALL	AddToBuffer	
+
+;Carry flag
+		POP		DE						; get flags so we can test
+;		POP		DE						; save flags so we can test later
+		LD		A,31H					; Assume flag set
+		BIT		0,E						; test it
+		JP		NZ,CarryFlagSet
+		DEC		A						; Not Set  - ASCII 0
+CarryFlagSet:
+		CALL	AddToBuffer	
+		CALL	AddSpaceToBuffer
+
+		RET
+		
+		
+LineBreak:
+		LD		A,(arg1)
+		AND		0F0H
+		CP		0
+		CALL	Z,DELAY
+		CALL	AddLFToBuffer
+		CALL	PrintBufferAndClear
+		RET
+		
+PutArgsinBuffer:
+		CALL	GetArg2
+		CALL	AddByteToBuffer		; arg1 is in ACC
+		LD		A,02BH					; +
+;		LD		A,02DH					; -
+		CALL	AddToBuffer
+		CALL	GetArg1
+		CALL	AddByteToBufferSpace	; arg2 is in ACC
+		LD		A,03DH					; =
+		CALL	AddToBuffer
+		CALL	AddSpaceToBuffer
+		RET
+INIT:
+		LD		A,020H
+		CALL	COUT
+		CALL	DELAY
+		RET
+		
+InitArgs:
+		XOR		A
+		LD		(arg1),A
+		LD		(arg2),A
+		RET
+GetArg1:
+		LD		A,(arg1)
+		RET
+
+GetArg2:
+		LD		A,(arg2)
+		RET
+
+		
+arg1:	DS		1
+arg2:	DS		1
+		
+
+
+$INCLUDE buffer.asm	
+
+		ORG		(($/10H) + 1) * 10H	
+		DS		0100H
+TopOfStack:
---
+++ assemblerZ80_Test/Code/Work/testINC.asm
@@ -0,0 +1,154 @@
+;	testINC.asm
+
+PSTRING		EQU		0852H
+NEWLINE		EQU		088CH
+DELAY		EQU		0CE1H
+COUT		EQU		0861H
+		
+			
+
+			ORG		1800H
+Start:
+		CALL	ClearBuffer
+		CALL	INIT
+		CALL	NEWLINE
+		
+		CALL	InitArgs						; set value to Zero
+		LD		A,00
+Loop1:
+		LD		(arg1),A						; save counter
+
+		CALL	PutArgsinBuffer
+		CALL	PerformInc
+		CALL	LineBreak
+
+		LD		A,(arg1)
+		INC	A
+		LD		(arg1),A
+		JP		NZ,Loop1
+		
+
+		RST		38H
+		JP		Start
+;------------------------------------------		
+PerformInc:
+		CALL	GetArg1
+		INC		A
+
+
+
+
+		
+		
+RecordFlags:
+		PUSH	AF						; save Flag result
+;		PUSH	AF						; save Flag result
+		CALL	AddByteToBufferSpace	; result is in ACC
+		
+;sign flag		
+		POP		DE						; get flags so we can test
+		PUSH	DE						; save flags so we can test later
+		LD		A,31H					; Assume flag set
+		BIT		7,E						; test it
+		JP		NZ,SignFlagSet
+		DEC		A						; Not Set  - ASCII 0
+SignFlagSet:
+		CALL	AddToBuffer	
+		
+;zero flag
+		POP		DE						; get flags so we can test
+		PUSH		DE						; save flags so we can test later
+		LD		A,31H					; Assume flag set
+		BIT		6,E						; test it
+		JP		NZ,ZeroFlagSet
+		DEC		A						; Not Set  - ASCII 0
+ZeroFlagSet:
+		CALL	AddToBuffer	
+		
+;HalfCarry flag
+		POP		DE						; get flags so we can test
+		PUSH		DE						; save flags so we can test later
+		LD		A,31H					; Assume flag set
+		BIT		4,E						; test it
+		JP		NZ,HalfCarryFlagSet
+		DEC		A						; Not Set  - ASCII 0
+HalfCarryFlagSet:
+		CALL	AddToBuffer
+		
+;Overflow flag
+		POP		DE						; get flags so we can test
+		PUSH		DE						; save flags so we can test later
+		LD		A,31H					; Assume flag set
+		BIT		2,E						; test it
+		JP		NZ,OverflowFlagSet
+		DEC		A						; Not Set  - ASCII 0
+OverflowFlagSet:
+		CALL	AddToBuffer	
+
+;N flag
+		POP		DE						; get flags so we can test
+		PUSH		DE						; save flags so we can test later
+		LD		A,31H					; Assume flag set
+		BIT		1,E						; test it
+		JP		NZ,nFlagSet
+		DEC		A						; Not Set  - ASCII 0
+nFlagSet:
+		CALL	AddToBuffer	
+
+;Carry flag
+		POP		DE						; get flags so we can test
+;		POP		DE						; save flags so we can test later
+		LD		A,31H					; Assume flag set
+		BIT		0,E						; test it
+		JP		NZ,CarryFlagSet
+		DEC		A						; Not Set  - ASCII 0
+CarryFlagSet:
+		CALL	AddToBuffer	
+		CALL	AddSpaceToBuffer
+
+		RET
+		
+		
+LineBreak:
+		LD		A,(arg1)
+		AND		0F0H
+		CP		0
+		CALL	Z,DELAY
+		CALL	AddLFToBuffer
+		CALL	PrintBufferAndClear
+		RET
+		
+PutArgsinBuffer:
+		CALL	GetArg1
+		CALL	AddByteToBuffer		; arg1 is in ACC
+		LD		A,09H					; TAB
+		CALL	AddToBuffer
+;		CALL	AddSpaceToBuffer
+		RET
+INIT:
+		LD		A,020H
+		CALL	COUT
+		CALL	DELAY
+		RET
+		
+InitArgs:
+		XOR		A
+		LD		(arg1),A
+		RET
+GetArg1:
+		LD		A,(arg1)
+		RET
+
+
+
+		
+arg1:	DS		1
+
+		
+
+
+$INCLUDE buffer.asm	
+
+		ORG		(($/10H) + 1) * 10H	
+		DS		0100H
+TopOfStack:
